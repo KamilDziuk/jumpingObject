bd1858ded903bc9117494f90669b5a60b1abec5d
kamildziuk <kontakt@kamildziuk.pl>
2024-06-18 20:13:41 +0200

    Adding a method for counting points. Adding a gif (jumping Object)

--- js/script.js
+++ js/script.js
@@ -2,14 +2,35 @@
 
 
 
-
+let counter = document.querySelector(".counter");
 let jumpingObject = document.querySelector(".jumpingObject");
 let blockMoving = document.querySelector(".blockMoving");
-jumpingObject.style.left = "490px"
-jumpingObject.style.top = "490px"
+jumpingObject.style.left = "480px"
+jumpingObject.style.top = "472px"
 blockMoving.style.left = "10px"
-blockMoving.style.top = "468px"
+blockMoving.style.top = "405px"
+let point = 0;
+pointCounter = () =>
+    {
+        if(point <= 12000){
+        counter.innerHTML = `Points:${point}`;
+        setInterval(pointCounter,1700)
+        point ++;
+        }
+    }
 
+    pointCounter();
+jumpingObject.addEventListener("click", (event) =>{
+
+    
+    jumpingObject.style.top =  "400px"
+    setTimeout( () =>
+    {
+     
+    jumpingObject.style.top =  "472px"
+    },300)
+
+});
 document.addEventListener("keydown", (event) =>{
   
   
@@ -23,7 +44,7 @@ document.addEventListener("keydown", (event) =>{
       setTimeout( () =>
       {
        
-      jumpingObject.style.top =  "490px"
+      jumpingObject.style.top =  "472px"
       },300)
        
           }
@@ -38,9 +59,10 @@ setInterval(() =>
     let currentLeft2 = parseInt(blockMoving.style.left);
     blockMoving.style.left = `${currentLeft2 + 5}px`
 
-    if(blockMoving.style.left == "490px"  && jumpingObject.style.top == "490px" )
+    if(blockMoving.style.left == "490px"  && jumpingObject.style.top == "472px" )
         {
-      alert("Game over")
+            pointCounter();
+      alert(`Game over, Points:${point}`)
          
         }
 
@@ -56,4 +78,4 @@ setInterval(() =>
             }
   
               
-},10)
+},1000)
