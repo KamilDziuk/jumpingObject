52924011c1d778dc89adb661f206a4221fd048e9
kamildziuk <kontakt@kamildziuk.pl>
2026-01-17 19:45:44 +0100

    feat: Change jump button image format to webp

--- src/scripts/mobileJumpButton.js
+++ src/scripts/mobileJumpButton.js
@@ -1,7 +1,7 @@
 export const mobileJumpButton = (jumpButton) => {
   jumpButton = document.createElement("img");
   jumpButton.className = "jumpButton";
-  jumpButton.src = `/assets/images/good_character.png`;
+  jumpButton.src = `/assets/images/jump_good_character.webp`;
   document.body.appendChild(jumpButton);
 
   return jumpButton;
