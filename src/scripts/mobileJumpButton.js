1930b0717a46ca3e9c49393b992e5802cce9456b
kamildziuk <kontakt@kamildziuk.pl>
2026-01-17 19:22:09 +0100

    feat: Add function to create jump button
    Add mobileJumpButton Function to create jump button

--- src/scripts/mobileJumpButton.js
+++ src/scripts/mobileJumpButton.js
@@ -1,1 +1,8 @@
+export const mobileJumpButton = (jumpButton) => {
+  jumpButton = document.createElement("img");
+  jumpButton.className = "jumpButton";
+  jumpButton.src = `/assets/images/good_character.png`;
+  document.body.appendChild(jumpButton);
 
+  return jumpButton;
+};
