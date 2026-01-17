43233a866a3f3e891517e0f94ee1d719b0a01b51
kamildziuk <kontakt@kamildziuk.pl>
2026-01-17 18:39:35 +0100

    fix: Fix badCharacter movement logic for small screens

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -42,7 +42,7 @@
           moveCounter = parseInt(badCharacter.style.right);
 
           window.innerWidth <= 500
-            ? (badCharacter.style.right = `${moveCounter + 4}%`)
+            ? (badCharacter.style.right = `${moveCounter + 1}%`)
             : (badCharacter.style.right = `${moveCounter + 1}%`);
         };
 
